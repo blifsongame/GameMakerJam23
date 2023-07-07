b38cfb78791375754ec28bda45e5dbe6f24e3b73
Ben Lifson <belifson@microsoft.com>
2023-07-07 15:50:06 -0700

    Make character that jumps at gap

---
+++ GMTK_GameJam_23/Assets/Game/Script/DumbPlayerManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+
+namespace BridgePlatformer {
+
+    // TODO: Do I nee to make this a constant?
+    public class DumbPlayerManager : MonoBehaviour
+    {
+        private static DumbPlayerManager instance;
+        public static DumbPlayerManager Instance
+		{
+            get
+			{
+                if (instance == null)
+				{
+                    instance = FindAnyObjectByType<DumbPlayerManager>();
+				}
+                return instance;
+			}
+		}
+
+
+    }
+}
