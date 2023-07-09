1026c5165ef927a278f962c38aa7be068693d9af
Ben Lifson <belifson@microsoft.com>
2023-07-08 22:12:47 -0700

    nimate flag

---
+++ GMTK_GameJam_23/Assets/StartButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+
+public class StartButton : MonoBehaviour
+{
+	[SerializeField]
+	private GoToLevelEntryPoint goToLevelEntryPoint;
+
+	public void OnButtonClick()
+	{
+		goToLevelEntryPoint.GoToNextLevel();
+	}
+}
